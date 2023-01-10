5243c273c9467cad54bd6dd61d9b04c3f467b195
trieu2201 <richdadtrieu@gmail.com>
2023-01-10 09:41:54 +0700

    Exercise_5

--- STM32/Core/Src/main.c
+++ STM32/Core/Src/main.c
@@ -48,7 +48,9 @@ int mil_500 = 1;
 
 const int MAX_LED = 4;
 int index_led = 0;
-int led_buffer [4] = {1, 2, 3, 4};
+int led_buffer [4] = {0, 0, 0, 0};
+
+int hour = 15, minute = 8, second = 50;
 /* USER CODE END PV */
 
 /* Private function prototypes -----------------------------------------------*/
@@ -57,6 +59,8 @@ static void MX_TIM2_Init(void);
 static void MX_GPIO_Init(void);
 /* USER CODE BEGIN PFP */
 void update7SEG(int);
+void updateClockBuffer(void);
+void display7SEG(int);
 /* USER CODE END PFP */
 
 /* Private user code ---------------------------------------------------------*/
@@ -102,7 +106,20 @@ int main(void)
   while (1)
   {
     /* USER CODE END WHILE */
-
+	  second++;
+	  if(second >= 60) {
+		  second = 0;
+		  minute++;
+	  }
+	  if(minute >= 60) {
+		  minute = 0;
+		  hour++;
+	  }
+	  if(hour >= 24) {
+		  hour = 0;
+	  }
+	  updateClockBuffer();
+	  HAL_Delay(1000);
     /* USER CODE BEGIN 3 */
   }
   /* USER CODE END 3 */
@@ -268,22 +285,6 @@ void display7SEG(int num) {
 		}
 }
 
-int led = 0;
-void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef*htim) {
-	mil_1000--; mil_500--;
-
-	if(mil_500 == 0) {
-		mil_500 = 50;
-
-		update7SEG(index_led++);
-		if(index_led == MAX_LED) index_led = 0;
-	}
-	if(mil_1000 == 0) {
-		mil_1000 = 100;
-		HAL_GPIO_TogglePin(DOT_PORT, DOT);
-		HAL_GPIO_TogglePin(RLED_PORT, RLED1);
-	}
-}
 void update7SEG(int index) {
 	HAL_GPIO_WritePin(EN_PORT, EN0 | EN1 | EN2 | EN3, 1);
 	display7SEG(led_buffer[index]);
@@ -305,6 +306,30 @@ void update7SEG(int index) {
 		break;
 	}
 }
+
+void updateClockBuffer() {
+	led_buffer[0] = hour / 10;
+	led_buffer[1] = hour % 10;
+	led_buffer[2] = minute / 10;
+	led_buffer[3] = minute % 10;
+}
+
+void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef*htim) {
+	mil_1000--; mil_500--;
+
+	if(mil_500 == 0) {
+		mil_500 = 50;
+
+		update7SEG(index_led++);
+		if(index_led == MAX_LED) index_led = 0;
+	}
+	if(mil_1000 == 0) {
+		mil_1000 = 100;
+		HAL_GPIO_TogglePin(DOT_PORT, DOT);
+		HAL_GPIO_TogglePin(RLED_PORT, RLED1);
+	}
+}
+
 /* USER CODE END 4 */
 
 /**
